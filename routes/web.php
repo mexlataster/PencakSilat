617e84dcf9d9d3bda70528d7d618fd39fd627069
Mex Lataster <kip.kippeneieren@gmail.com>
2017-06-07 18:07:05 +0200

    Bezig met een andere Contact Pagina
    Ik ben nu bezig met een andere Contact Pagina maar die hou ik even UNDER CONSTRUCTION..
    Gaat later wel aan gewerkt worden!

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::post('/postInsert', 'CrudController@postInsert');
 Route::get('image-gallery', 'ImageGalleryController@index');
 Route::post('image-gallery', 'ImageGalleryController@upload');
 Route::delete('image-gallery/{id}', 'ImageGalleryController@destroy');
+Route::get('contact',
+  ['as' => 'contact', 'uses' => 'AboutController@create']);
+Route::post('contact',
+  ['as' => 'contact_store', 'uses' => 'AboutController@store']);
