413a65033ddab4345e037ab95a9612bf8249edb3
Mark Glimmerveen <mark.glimmerveen@hotmail.com>
2017-06-13 11:34:04 +0200

    Contact Page Changes
    Style toegevoegd aan de Contact Page

--- resources/views/user/getInsert.blade.php
+++ resources/views/user/getInsert.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
 <html>
   <head>
     <meta charset="utf-8">
@@ -7,51 +8,6 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css"
   integrity="sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ=="
   crossorigin=""/>
-    <style type="text/css">
-                html ,body {
-                  width: 600px;
-                  margin: 0 auto;
-                  padding: 10px;
-                }
-                .container{
-                  background: #fff;
-                  box-shadow: 0px, 0px, 10px, gray;
-                  padding:10px;
-                }
-                .message{
-                  min-height:100px;
-
-                }
-
-                .information{
-                	float: left;
-                  background-color: #fff;
-                  color: #636b6f;
-                  font-family: 'Raleway', sans-serif;
-                  font-weight: 100;
-                }
-                h1, td{
-                  background-color: #fff;
-                  color: #636b6f;
-                  font-family: 'Raleway', sans-serif;
-                  font-weight: 100;
-                }
-                img.head{
-                	padding:2px;
-                  border-radius:10px;
-                  margin-left:15px;
-                	width:200px;
-                	background-color:rgba(255,255,255, 0.1);
-                }
-
-
-
-
-
-
-
-
-    </style>
   </head>
 
   <script src='https://api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.js'></script>
@@ -68,28 +24,28 @@
 
 
         <div class="container">
-          <h1>Contact Page</h1>
-          <hr>
-            <form action="{{ url('postInsert')}}" method="POST">
-               {{ csrf_field() }}
-                  <table>
-                      <tr>
-                          <td>Name</td>
-                          <td><input type="text" name="name" id="name"></td>
-                      </tr>
-                      <tr>
-                          <td>Email adress</td>
-                          <td><input type="email" name="email" id="email"></td>
-                      </tr>
-                      <tr>
-                          <td>Message</td>
-                          <td><input type="textarea" name="message" id="message" class="message"></td>
-                      </tr>
-                  </table>
-                  <hr>
-                    <input type="submit" value="Submit">
-            </form>
-                <div id='map' style='width: 600px; height: 500px;, margin-top:40px;'></div>
+          <div class="text-input">
+            <h1>Contact Page</h1>
+              <form action="{{ url('postInsert')}}" method="POST">
+                 {{ csrf_field() }}
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td><input type="text" name="name" id="name"></td>
+                        </tr>
+                        <tr>
+                            <td>Email adress</td>
+                            <td><input type="email" name="email" id="email"></td>
+                        </tr>
+                        <tr>
+                            <td>Message</td>
+                            <td><textarea rows="4" cols="50" name="message" id="message" class="message"></textarea></td>
+                        </tr>
+                    </table>
+                      <input type="submit" value="Submit" class="submit-button">
+              </form>
+              </div>
+                <div id='map'></div>
                   <script>
                       mapboxgl.accessToken = 'pk.eyJ1IjoibWV4aWVtb25zdGVyIiwiYSI6ImNqM2UyeHl0aTAwMDgzM3A3a3p5M3JxamgifQ.V6MorPyKRZCeOo_AbsckpQ';
                       var map = new mapboxgl.Map({
@@ -97,6 +53,8 @@
                       style: 'mapbox://styles/mapbox/satellite-streets-v9'
                       });
                   </script>
+                </div>
+          
         </div>
   </body>
 </html>
