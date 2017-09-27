5cd9584b19cc681bd701fc95926b66a5872a6d7a
Dilie <dilie_90@hotmail.com>
2017-09-27 13:42:21 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,11 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('user_type');
             $table->string('adres');
             $table->string('woontplaats');
             $table->string('graduatie');
-            $table->string('graduatie-behaald');
+            $table->string('graduatie_behaald');
             $table->string('foto-id');
             $table->rememberToken();
             $table->timestamps();
